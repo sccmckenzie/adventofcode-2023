c9145cd17cef98de61912d2620b108a7d565464b
scottmckenzie <sccmckenzie@gmail.com>
2023-12-26 15:51:24 -0600

    day5: partially working

--- day5/main.py
+++ day5/main.py
@@ -1,43 +1,88 @@
 import re
 import polars as pl
 
-input_path = 'input_partial.txt'
-seeds = pl.Series(dtype=pl.UInt32)
-current_mapping = None
-almanac = pl.DataFrame(
+# predefine variables, mostly for first iteration
+input_path = 'input_demo.txt'
+seeds = pl.Series(dtype=pl.UInt64)
+map_coord = {}
+map_current = None
+
+# read seeds and mapping categories
+# not fully parsing yet
+with open(input_path, 'r') as file:
+    for num, line in enumerate(file, 1):
+        line = line.strip()
+
+        match_seed = re.fullmatch("^seeds:.+$", line)
+        match_mapname = re.fullmatch("^.+\\smap:$", line)
+        match_intonly = re.fullmatch("^[\\d\\s]+$", line)
+
+        if match_seed is not None:
+            seeds = pl.Series("seed", re.findall("\\d+", line)).cast(pl.UInt64)
+        elif match_mapname is not None:
+            map_current = line.replace(' map:', '')
+            map_coord[map_current] = {"start": num, "end": num}
+        elif match_intonly is not None:
+            map_coord[map_current]["end"] = num
+
+# create enum object (now that we have all possible values)
+# e.g. 'seed-to-soil', 'soil-to-fertilizer'
+map_enum = pl.Enum(list(map_coord.keys()))
+
+# predefine empty almanac for first iteration
+almanac = pl.LazyFrame(
     {
-        "mapping": pl.Series(dtype=pl.Categorical),
-        "coord_dest": pl.Series(dtype=pl.UInt32),
-        "coord_source": pl.Series(dtype=pl.UInt32),
+        "mapping": pl.Series(dtype=map_enum),
+        "dest_lower": pl.Series(dtype=pl.UInt64),
+        "source_lower": pl.Series(dtype=pl.UInt64),
+        "length": pl.Series(dtype=pl.UInt64)
     }
 )
-with open(input_path, 'r') as file:
-    for line in file:
-        line = line.strip()
-        if re.fullmatch("^seeds:.+$", line) is not None:
-            seeds = pl.Series("seed", re.findall("\\d+", line)).cast(pl.UInt32)
-        elif re.fullmatch("^.+\\smap:$", line) is not None:
-            current_mapping = pl.Series("mapping",
-                                        [re.match("^\\S+", line).group(0)], dtype=pl.Categorical)
-        elif re.fullmatch("^[\\d\\s]+$", line) is not None:
-            line_num = [int(x) for x in re.findall("\\d+", line)]
-            coord_dest = pl.Series(
-                "coord_dest", range(line_num[0], line_num[0] + line_num[2])).cast(pl.UInt32)
-            coord_source = pl.Series(
-                "coord_source", range(line_num[1], line_num[1] + line_num[2])).cast(pl.UInt32)
-            almanac = pl.concat(
-                [
-                    almanac,
-                    pl.DataFrame(
-                        [
-                            pl.Series("coord_dest", coord_dest),
-                            pl.Series("coord_source", coord_source)
-                        ]
-                    ).select(current_mapping,
-                             pl.col("coord_dest"),
-                             pl.col("coord_source"))
-                ]
-            )
+
+for key in map_coord:
+    start = map_coord[key]["start"]
+    end = map_coord[key]["end"]
+    almanac = pl.concat(
+        [
+            almanac,
+            pl.scan_csv(input_path,
+                        has_header=False,
+                        separator=' ',
+                        skip_rows=start,
+                        n_rows=(end - start),
+                        new_columns=['dest_lower', 'source_lower', 'length'],
+                        dtypes=[pl.Utf8] * 3)
+            .select(
+                pl.Series("mapping", [key], dtype=map_enum),
+                pl.all().cast(dtype=pl.UInt64))
+        ]
+    )
+
+almanac = almanac.with_columns(
+    (pl.col("source_lower") + pl.col("length") - 1).alias("source_upper"))
+
+
+
+(
+    pl.LazyFrame(seeds)
+    .join(
+        almanac.filter(pl.col("mapping") == pl.Series(
+            ["seed-to-soil"], dtype=map_enum)),
+        how="cross"
+    )
+    .with_columns(
+        ((pl.col("seed") >= pl.col("source_lower")) &
+         (pl.col("seed") <= pl.col("source_upper")))
+        .alias("is_captured")
+    )
+    .with_columns(
+        pl.when(pl.col("is_captured")).then(pl.col("dest_lower") + pl.col("seed") - pl.col("source_lower")).alias("dest")
+    )
+    .group_by(["seed", "mapping"])
+    .agg(pl.col("dest").max().alias("soil"))
+    .with_columns(soil=pl.when(pl.col("soil").is_null()).then(pl.col("seed")).otherwise(pl.col("soil")))
+    .collect()
+)
 
 # full_mapping = pl.DataFrame(seeds).join(
 #     almanac.filter(pl.col("mapping") == "seed-to-soil").rename({"coord_source": "seed"}),
