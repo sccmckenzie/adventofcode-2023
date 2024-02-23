e83b51f93c8fcc89de1b1f3319a1b2bd318021c6
scottmckenzie <sccmckenzie@gmail.com>
2024-02-23 13:17:02 -0700

    remove unnecessary classes

--- day6/day6/src/main/java/org/sccmckenzie/aoc2023/day6/Main.java
+++ day6/day6/src/main/java/org/sccmckenzie/aoc2023/day6/Main.java
@@ -4,7 +4,6 @@ package org.sccmckenzie.aoc2023.day6;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
